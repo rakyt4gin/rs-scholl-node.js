916c6bcec51d7cc71d422b81f04715aff17ecee1
Andrei Skrinnik <rakyt4gin@gmail.com>
2021-11-07 18:02:55 +0300

    feat: realize all ciphers

--- getInformationFromCLI.js
+++ getInformationFromCLI.js
@@ -4,13 +4,19 @@ class GetInformationFromCLI {
 
   data = {}
 
-  exportInformation() {
+
+  getInformation() {
+
+    this.data.configFlag = argv[argv.indexOf('-c')]
     this.data.config = argv[argv.indexOf('-c') + 1]
+    this.data.inputFlag = argv[argv.indexOf('-i')]
     this.data.input = argv[argv.indexOf('-i') + 1].slice(2)
+    this.data.outputFlag = argv[argv.indexOf('-o')]
     this.data.output = argv[argv.indexOf('-o') + 1].slice(2)
     return this.data
+
   }
 
 }
 
-module.exports = (new GetInformationFromCLI()).exportInformation()
+module.exports = (new GetInformationFromCLI()).getInformation()
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
+const process = require('process')
 const path = require('path')
-const { config } = require('process')
 const { Readable, Writable } = require('stream')
 const getInformationFromCLI = require('./getInformationFromCLI')
 
-class streamRead extends Readable {
+class StreamRead extends Readable {
   constructor(data) {
     super()
     this.readStream = fs.createReadStream(path.join(__dirname, data.input))
@@ -15,6 +15,43 @@ class streamRead extends Readable {
     this.readStream.on('data', chunk => {
       new CipherChoice(chunk.toString(), getInformationFromCLI.config)
     })
+    this.readStream.on('error', (err) => {
+      process.stderr.write(`Упс... Фаила ${getInformationFromCLI.input} не существет. Прошу вас написать ваше сообщение здесь\n`)
+      process.stdin.on('readable', () => {
+        new CipherChoice(process.stdin.read().toString(), getInformationFromCLI.config)
+      })
+    })
+  }
+
+}
+
+class StreamWrite extends Writable {
+  constructor(data) {
+
+    super()
+    this.data = data
+
+    this.readStream = fs.createReadStream(path.join(__dirname, getInformationFromCLI.output))
+
+    this.readStream.on('data', chunk => {
+      this.data += chunk.toString()
+    })
+    this.readStream.on('end', () => {
+      this.writeStream = fs.createWriteStream(path.join(__dirname, getInformationFromCLI.output))
+      this._write(this.data.trim())
+    })
+    this.readStream.on('error', () => {
+      process.stderr.write(`Фаила ${getInformationFromCLI.output} не существует. Конечный результат шифрования представлен ниже\n`)
+      process.stdout.write(this.data.trim())
+    })
+
+
+
+    // 
+  }
+  _write(data) {
+    this.writeStream.write(data)
+    this.writeStream.end()
   }
 
 }
@@ -29,9 +66,95 @@ class CipherChoice {
   choise() {
     if (this.config[0][0] == 'C') {
       new CaesarCipher(this.data, this.config)
+    } else if (this.config[0][0] == 'R') {
+      new ROT8Cipher(this.data, this.config)
+    } else if (this.config[0][0] == 'A') {
+      new AtbashCipher(this.data, this.config)
     } else {
-      console.log('enen')
+      new StreamWrite(this.data.join(''))
     }
+  }
+}
+
+class AtbashCipher {
+  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  constructor(data, config) {
+    this.encoding(data, config)
+  }
+
+  encoding(data, config) {
+    let arr = []
+    data.forEach(item => {
+      if (this.alphabet.includes(item)) {
+        arr.push(this.alphabet[(this.alphabet.length - this.alphabet.indexOf(item)) - 1])
+      }
+      else if (this.ALPHABET.includes(item)) {
+        arr.push(this.ALPHABET[(this.ALPHABET.length - this.ALPHABET.indexOf(item)) - 1])
+      } else {
+        arr.push(item)
+      }
+
+    })
+    config.shift()
+    return new CipherChoice(arr.join(''), config.join('-'))
+  }
+}
+
+class ROT8Cipher {
+  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  constructor(data, config) {
+    if (config[0][1] == '1') {
+      this.encoding(data, config)
+    } else if (config[0][1] == '0') {
+      this.deconding(data, config)
+    }
+  }
+  encoding(data, config) {
+    let arr = []
+    data.forEach(item => {
+      if (this.alphabet.includes(item)) {
+        if (this.alphabet.indexOf(item) > this.alphabet.length - 9) arr.push(this.alphabet[this.alphabet.indexOf(item) + 8 - this.alphabet.length])
+        else arr.push(this.alphabet[this.alphabet.indexOf(item) + 8])
+      } else if (this.ALPHABET.includes(item)) {
+        if (this.ALPHABET.indexOf(item) > this.ALPHABET.length - 9) arr.push(this.ALPHABET[this.ALPHABET.indexOf(item) + 8 - this.ALPHABET.length])
+        else arr.push(this.ALPHABET[this.ALPHABET.indexOf(item) + 8])
+      }
+      else {
+        arr.push(item)
+      }
+    })
+    config.shift()
+    return new CipherChoice(arr.join(''), config.join('-'))
+  }
+
+  deconding(data, config) {
+    let arr = []
+    data.forEach(item => {
+      if (this.alphabet.includes(item)) {
+        if (this.alphabet.indexOf(item) < 8) {
+          arr.push(this.alphabet[this.alphabet.indexOf(item) - 8 + this.alphabet.length])
+        }
+        else {
+          arr.push(this.alphabet[this.alphabet.indexOf(item) - 8])
+        }
+
+      } else if (this.ALPHABET.includes(item)) {
+        if (this.ALPHABET.indexOf(item) < 8) {
+          arr.push(this.ALPHABET[this.ALPHABET.indexOf(item) - 8 + this.ALPHABET.length])
+        }
+        else {
+          arr.push(this.ALPHABET[this.ALPHABET.indexOf(item) - 8])
+        }
+      }
+
+      else {
+        arr.push(item)
+      }
+    })
+    config.shift()
+    return new CipherChoice(arr.join(''), config.join('-'))
   }
 }
 
@@ -48,38 +171,44 @@ class CaesarCipher {
   }
 
   encoding(data, config) {
+    let arr = []
     data.forEach(item => {
 
       if (this.alphabet.includes(item)) {
-        if (item == 'z') data.splice(data.indexOf(item), 1, 'a')
-        else data.splice(data.indexOf(item), 1, this.alphabet[this.alphabet.indexOf(item) + 1])
+        if (item == 'z') arr.push('a')
+        else arr.push(this.alphabet[this.alphabet.indexOf(item) + 1])
       } else if (this.ALPHABET.includes(item)) {
-        if (item == 'Z') data.splice(data.indexOf(item), 1, 'A')
-        else data.splice(data.indexOf(item), 1, this.ALPHABET[this.ALPHABET.indexOf(item) + 1])
+        if (item == 'Z') arr.push('A')
+        else arr.push(this.ALPHABET[this.ALPHABET.indexOf(item) + 1])
+      } else {
+        arr.push(item)
       }
 
     })
     config.shift()
-    return new CipherChoice(data.join(''), config.join('-'))
+    return new CipherChoice(arr.join(''), config.join('-'))
   }
 
   deconding(data, config) {
+    let arr = []
     data.forEach(item => {
 
       if (this.alphabet.includes(item)) {
-        if (item == 'a') data.splice(data.indexOf(item), 1, 'z')
-        else data.splice(data.indexOf(item), 1, this.alphabet[this.alphabet.indexOf(item) - 1])
+        if (item == 'a') arr.push('z')
+        else arr.push(this.alphabet[this.alphabet.indexOf(item) - 1])
       } else if (this.ALPHABET.includes(item)) {
-        if (item == 'A') data.splice(data.indexOf(item), 1, 'Z')
-        else data.splice(data.indexOf(item), 1, this.ALPHABET[this.ALPHABET.indexOf(item) - 1])
+        if (item == 'A') arr.push('Z')
+        else arr.push(this.ALPHABET[this.ALPHABET.indexOf(item) - 1])
+      } else {
+        arr.push(item)
       }
 
     })
     config.shift()
-    return new CipherChoice(data.join(''), config.join('-'))
+    return new CipherChoice(arr.join(''), config.join('-'))
   }
 }
 
-(new streamRead(getInformationFromCLI))._read()
+(new StreamRead(getInformationFromCLI))._read()
 
 
