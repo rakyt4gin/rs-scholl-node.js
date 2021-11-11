84fb66ce2bfb20a1b46ffe0b103260b991ee967d
Andrei Skrinnik <rakyt4gin@gmail.com>
2021-11-11 17:17:53 +0300

    fix bugs with input/output arguments

--- getInformationFromCLI.js
+++ getInformationFromCLI.js
@@ -12,17 +12,24 @@ class GetInformationFromCLI {
       if (argv[argv.indexOf('-i') + 1][0] != '-') {
         this.data.input = argv[argv.indexOf('-i') + 1].slice(2)
       }
+      else {
+        this.data.input = ''
+      }
     } else {
-      this.data.input = ''
+      this.data.output = ''
     }
     this.data.outputFlag = argv[argv.indexOf('-o')]
     if (argv[argv.indexOf('-o') + 1]) {
       if (argv[argv.indexOf('-o') + 1][0] != '-') {
         this.data.output = argv[argv.indexOf('-o') + 1].slice(2)
       }
+      else {
+        this.data.output = ''
+      }
     } else {
       this.data.output = ''
     }
+    console.log(this.data);
     return this.data
   }
 
